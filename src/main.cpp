be8fefe94a3d6b3cc9c61c3c28f561614d92a948
ddddincn <ddddincn@163.com>
2024-12-21 11:08:37 +0800

    add third party

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 std::string project_name = "project";
 // std::string project_version = "0.1.0";
 std::string cpp_standard = "17";
+std::string third_party = "";
 
 void project_config(int argc, char **argv) {
   if (argc >= 2) {
@@ -26,11 +29,27 @@ void project_config(int argc, char **argv) {
   // if(!cin_project_version.empty())
   //   project_version = cin_project_version;
 
-  std::cout << "C++ standand (defaults to " << cpp_standard << ") :";
+  std::cout << "C++ standard (defaults to " << cpp_standard << ") :";
   std::string cin_cpp_standard;
   std::getline(std::cin, cin_cpp_standard);
   if (!cin_cpp_standard.empty())
     cpp_standard = cin_cpp_standard;
+
+  std::cout << "C++ third party (defaults to empty, please split by ' ') :";
+  std::string cin_third_party;
+  std::getline(std::cin, cin_third_party);
+  if (!cin_third_party.empty())
+    third_party = cin_third_party;
+}
+
+std::vector<std::string> splitString(const std::string &input) {
+  std::vector<std::string> result;
+  std::stringstream ss(input);
+  std::string temp;
+  while (ss >> temp) {
+    result.push_back(temp);
+  }
+  return result;
 }
 
 int main(int argc, char **argv) {
@@ -69,10 +88,28 @@ int main(int argc, char **argv) {
                  ")\nset(CMAKE_CXX_STANDARD_REQUIRED "
                  "ON)\n\ninclude_directories(${CMAKE_SOURCE_DIR}/"
                  "include)\n\naux_source_directory(${CMAKE_SOURCE_DIR}/src "
-                 "PROJECT_SOURCES)\n\n# if you "
-                 "need\n# find_package()\n\nadd_executable(${PROJECT_NAME} "
-                 "${PROJECT_SOURCES})\n\n# if you need\n# "
-                 "target_link_libraries(${PROJECT_NAME} ...)";
+                 "PROJECT_SOURCES)\n\n";
+    if (third_party.empty()) {
+      cmake << "# if you need\n# find_package()\n\n";
+    } else {
+      std::vector<std::string> third_parties = splitString(third_party);
+      for (auto &tp : third_parties) {
+        std::string find_package = "find_package(" + tp + " REQUIRED)\n";
+        std::string if_found = "if(" + tp + "_FOUND)\n";
+        std::string message = "\tmessage(STATUS \"" + tp +
+                              " Found (version: ${" + tp + "_VERSION})\")\n";
+        std::string include_dir =
+            "\tinclude_directories(${" + tp + "_INCLUDE_DIRS})\n";
+        std::string link_dir =
+            "\tlink_directories(${" + tp + "_LIBRARY_DIRS})\n";
+        std::string definition =
+            "\tadd_definitions(${" + tp + "_DEFINITIONS})\n";
+        cmake << find_package << if_found << message << include_dir << link_dir
+              << definition << "endif()\n\n";
+      }
+    }
+    cmake << "add_executable(${PROJECT_NAME} ${PROJECT_SOURCES})\n\n# if you "
+             "need\n# target_link_libraries(${PROJECT_NAME} ...)";
     cmake.close();
   }
   std::cout << "generate " + project_name + " finish" << std::endl;
